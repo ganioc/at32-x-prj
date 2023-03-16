91a6eca7a3981c7c7e6927f2e4524ad8354acea2
yangjun <yangjun@nanchao.org>
2023-03-16 15:09:05 +0800

    mv cmsis models

--- build/cmsis/subdir.mk
+++ build/cmsis/subdir.mk
@@ -4,10 +4,11 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 S_SRCS += \
-../cmsis/startup_at32f415.s 
+./models/$(MODEL)/cmsis/startup_at32f415.s 
 
 C_SRCS += \
-../cmsis/system_at32f415.c 
+./models/$(MODEL)/cmsis/system_at32f415.c 
+
 
 OBJS += \
 ./build/cmsis/startup_at32f415.o \
@@ -21,14 +22,14 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-./build/cmsis/%.o: ../cmsis/%.s ./build/cmsis/subdir.mk
+./build/cmsis/%.o: ./models/$(MODEL)/cmsis/%.s ./build/cmsis/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU Arm Cross Assembler'
 	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -O0 -ffunction-sections  -g -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-./build/cmsis/%.o: ../cmsis/%.c ./build/cmsis/subdir.mk
+./build/cmsis/%.o: ./models/$(MODEL)/cmsis/%.c ./build/cmsis/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU Arm Cross C Compiler'
 	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -O0 -ffunction-sections  -g -DAT_START_F415_V1 -DAT32F415RCT7 -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DUSE_STDPERIPH_DRIVER -I"../include" -I"../include/libraries/drivers/inc" -I"../include/libraries/cmsis/cm4/core_support" -I"../include/libraries/cmsis/cm4/device_support" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
